63a32acf745f474dfded0c2852f81809f5da7f20
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-10-19 17:17:55 -0300

    Score + update_score function

--- Game.gd
+++ Game.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var score = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,10 @@ func _physics_process(delta):
 	pass
 #	$LeftSideNoteSpawner.spawn()
 #	$RightSideNoteSpawner.spawn()
+
+
+func update_score(n):
+	score += n
 
 
 func _on_RightNoteTimer_timeout():
