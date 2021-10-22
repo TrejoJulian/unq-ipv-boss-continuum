ff20b9215f0b5b6ca9efb21c6ae670559ec46a78
lisar <lisar.3467@gmail.com>
2021-10-21 23:01:05 -0300

    se agrega previous score en el menu y fix al margin del menu

--- Menu.gd
+++ Menu.gd
@@ -1,6 +1,12 @@
 extends Control
 
 
+func _ready():
+	if GameStatus.score == 0:
+		$Background/VBoxContainer/HBoxContainer/Score.hide()
+	else:
+		$Background/VBoxContainer/HBoxContainer/Score.text = "Previous Score: " + str(GameStatus.score)
+
 func _on_TextureButton_pressed():
 	Global.goto_scene("res://Game.tscn")
 
