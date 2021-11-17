2de2381b34827f9f24e8b23e706261603f90a5d1
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-11-17 00:43:52 -0300

    Mapa 1 terminado + move timer personalizado

--- Game.gd
+++ Game.gd
@@ -2,6 +2,7 @@ extends Node
 export var map_path:String
 export var track:AudioStream
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GameData.initialize()
@@ -12,10 +13,17 @@ func _ready():
 	$AudioStreamPlayer.stream = load(Global.current_level.track)
 	$AudioStreamPlayer.play()
 
+
 func _on_HealthTimer_timeout():
 	if GameData.current > 1:
 		GameData.current -= 1
 
+
 func end_level():
+	GameStatus.set_score(GameData.score)
+	Global.goto_scene("res://menu/Menu.tscn")
+
+
+func _on_AudioStreamPlayer_finished():
 	GameStatus.set_score(GameData.score)
 	Global.goto_scene("res://menu/Menu.tscn")
--- tool/MapTool.gd
+++ tool/MapTool.gd
@@ -2,10 +2,11 @@ extends Node
 
 var left_timer_uptime:float = 0.0
 var right_timer_uptime:float  = 0.0
+var move_timer_uptime:float  = 0.0
 var left_timer_timeouts = []
 var right_timer_timeouts = []
+var move_timer_timeouts = []
 export var timer_interval = 0.05
-
 
 
 func _on_LeftTimer_timeout():
@@ -15,6 +16,11 @@ func _on_LeftTimer_timeout():
 func _on_RightTimer_timeout():
 	right_timer_uptime = right_timer_uptime + timer_interval
 
+
+func _on_MoveTimer_timeout():
+	move_timer_uptime = move_timer_uptime + timer_interval
+
+
 func _unhandled_input(event):
 	if event.is_action_pressed("left_arrow_pressed", false):
 		left_timer_timeouts.append(left_timer_uptime)
@@ -22,7 +28,10 @@ func _unhandled_input(event):
 	elif event.is_action_pressed("right_arrow_pressed", false):
 		right_timer_timeouts.append(right_timer_uptime)
 		right_timer_uptime = 0
+	elif event.is_action_pressed("up", false):
+		move_timer_timeouts.append(move_timer_uptime)
+		move_timer_uptime = 0
 	elif event.is_action_pressed("down"):
 		print(left_timer_timeouts)
 		print(right_timer_timeouts)
-
+		print(move_timer_timeouts)
