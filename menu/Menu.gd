4074f2a94f87d5d26faf70bfe4e6da43c5e8e339
lisar <lisar.3467@gmail.com>
2021-11-19 17:06:19 -0300

    fix provisorio de audio y de que no se ven las flechas

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -2,9 +2,11 @@ extends Node
 
 onready var main_menu: Control = $VBoxContainer/MainMenu
 onready var levels_menu: Control = $VBoxContainer/LevelsMenu
+onready var bgm = $BackgroundPlayer
 
 func _ready():
 	main_menu.connect("go_to_levels", self, "_on_go_to_levels")
+	levels_menu.connect("go_to_game", self, "_on_go_to_game")
 	main_menu.show()
 	levels_menu.hide()
 	
@@ -14,4 +16,14 @@ func _on_go_to_levels():
 	levels_menu.show()
 
 func _on_ExitButton_pressed():
+	bgm.stop()
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
 	get_tree().quit()
+
+func _on_go_to_game(level_id):
+	Global.current_level = Global.levels[level_id]
+	bgm.stop()
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	Global.goto_scene("res://Game.tscn")
