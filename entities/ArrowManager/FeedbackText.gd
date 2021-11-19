99e2ad0b1fc42ce0b34f9719599af32293257d73
Julian D. Trejo <62217520+TrejoJulian@users.noreply.github.com>
2021-11-19 01:31:50 -0300

    Se añade efecto visual al texto de feedback

--- entities/ArrowManager/FeedbackText.gd
+++ entities/ArrowManager/FeedbackText.gd
@@ -4,12 +4,15 @@ onready var left_side_text_label = $LeftSideText
 onready var right_side_text_label = $RightSideText
 onready var left_text_timer = $LeftTextTimer
 onready var right_text_timer = $RightTextTimer
+onready var left_text_effect = $LeftTextEffect
+onready var right_text_effect = $RightTextEffect
+
 export var perfect_score:int = 3
 export var great_score:int   = 2
 export var okay_score:int    = 1
 export var perfect_bbcode_text:String = "[wave][rainbow freq=2.5 val=1.3]Perfect"
-export var great_bbcode_text:String = "[wave]Great"
-export var okay_bbcode_text:String = "Okay"
+export var great_bbcode_text:String = "[wave] Great"
+export var okay_bbcode_text:String = "   Ok"
 export var great_text_colour = Color("e2ef08")
 export var default_text_colour = Color("ffffff")
 
@@ -17,10 +20,12 @@ export var default_text_colour = Color("ffffff")
 
 func on_right_arrow_button_pressed(score):
 	_change_text(right_side_text_label,text_from_score(score))
+	right_text_effect.visible = true
 	right_text_timer.start()
 
 func on_left_arrow_button_pressed(score):
 	_change_text(left_side_text_label,text_from_score(score))
+	left_text_effect.visible = true
 	left_text_timer.start()
 
 func text_from_score(score):
@@ -47,8 +52,10 @@ func _change_text_colour(label,colour):
 
 func _on_RightTextTimer_timeout():
 	_change_text(right_side_text_label,"")
+	right_text_effect.visible = false
 
 
 func _on_LeftTextTimer_timeout():
 	_change_text(left_side_text_label,"")
-	
+	left_text_effect.visible = false
+
