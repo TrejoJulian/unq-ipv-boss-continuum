bbef0e03e50985a2302a0c2b1dc6a179e0639881
Julian D. Trejo <62217520+TrejoJulian@users.noreply.github.com>
2021-11-29 17:15:49 -0300

    se añade un fade away effect a las notas para que el jugador pueda ver que tan bien timeado apreto el boton antes de que desaparezcan las notas

--- entities/note/Note.gd
+++ entities/note/Note.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 class_name Note
 
-onready var animated_sprite = $AnimatedSprite
+export (PackedScene) var ghost_note_scene:PackedScene
 
+onready var animated_sprite = $AnimatedSprite
+var direction
 export var speed:float = 100
 
 var hit = false
@@ -16,6 +18,7 @@ func _physics_process(delta):
 
 #note_direction < 0 is left and > 0 is right.
 func initialize(pos, note_direction, is_up):
+	direction = note_direction
 	global_position = pos
 	if note_direction > 0 :
 		if(is_up):
@@ -36,7 +39,15 @@ func initialize(pos, note_direction, is_up):
 
 func destroy(score):
 	hit = true
+	spawnGhost()
 	queue_free()
+
+func spawnGhost():
+	var ghost_note = ghost_note_scene.instance()
+	
+	get_parent().add_child(ghost_note)
+	ghost_note.set_as_toplevel(true)
+	ghost_note.initialize(global_position, direction, animated_sprite.frames.get_frame("default",animated_sprite.frame))
 
 
 func bad_area_entered():
