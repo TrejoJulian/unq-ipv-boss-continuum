55f5f0036585a02cd150ddf6b0204160888b3054
Julian D. Trejo <62217520+TrejoJulian@users.noreply.github.com>
2021-12-07 15:39:35 -0300

    Se añade color al score, se remueve la streak y se fixean visuales de perfect

--- gui/GUI.gd
+++ gui/GUI.gd
@@ -4,14 +4,12 @@ signal level_exited()
 
 onready var health_bar = $HealthBar
 onready var score = $Score
-onready var streak = $Streak
 onready var popup_panel = $PopupPanel
 
 func _ready():
 	GameData.connect("changed", health_bar, "set_value")
 	GameData.connect("max_changed", health_bar, "set_max")
 	GameData.connect("score_changed", score, "set_value")
-	GameData.connect("streak_changed", streak, "set_value")
 
 
 func _unhandled_input(event):
