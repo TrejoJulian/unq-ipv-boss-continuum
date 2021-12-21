e6b2113bea5bd3c7709924269b8dcbbf8e8939df
lisar <lisar.3467@gmail.com>
2021-12-21 17:09:14 -0300

    se cambian a otro color los fuegos artificiales cada vez que se pierde el streak

--- entities/firework/FireworkManager.gd
+++ entities/firework/FireworkManager.gd
@@ -5,12 +5,11 @@ func _ready():
 	GameData.connect("streak_changed", self, "activate_fireworks")
 
 
-# reproduzco los fireworks una vez y si fallo el streak desactivo el loopeo, no los paro
 func activate_fireworks(new_streak):
 	for f in get_children():
 		if new_streak == 0 and f.is_playing():
-			f.set_loop(false)
+			f.set_all_loops(false)
 		if f.start_streak == new_streak:
 			if not f.is_playing():
 				f.play()
-			f.set_loop(true)
+			f.set_all_loops(true)
